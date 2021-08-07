5ff6085f3046b42d0271806f9db68886614d7514
Rohejul Islam <islamrohijulr@gmail.com>
2021-08-07 15:37:17 +0600

    Added screens & created root navigation

--- ReduxExampleApp/App.js
+++ ReduxExampleApp/App.js
@@ -2,36 +2,17 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import RootNavigator from './navigation/RootNavigator';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={styles.screen}>
-      <View>
-        <Text>Redux Example App</Text>
-      </View>
-    </SafeAreaView>
+    <RootNavigator />
   );
 };
 
