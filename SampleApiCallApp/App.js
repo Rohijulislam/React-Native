e29c1afa50ae36fe528d81a35c4d6828dde2a238
Rohejul Islam <islamrohijulr@gmail.com>
2021-08-09 14:54:10 +0600

    #3 Create empty project on networking

---
+++ SampleApiCallApp/App.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.screen}>
+      <View >
+        <Text>Sample Api Call App</Text>
+      </View>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default App;
