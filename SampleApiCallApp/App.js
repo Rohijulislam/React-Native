91e9d6366af51c3feaefefc664b8236fc5acd6ca
Rohejul Islam <islamrohijulr@gmail.com>
2021-08-10 00:42:27 +0600

    #3 Done integrating axios networking in basic single page app

--- SampleApiCallApp/App.js
+++ SampleApiCallApp/App.js
@@ -1,40 +1,56 @@
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+//import React in our code.
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+//import all the components we are going to use.
+import { getDataUsingSimpleGetCall, postDataUsingSimplePostCall, multipleRequestsInSingleCall } from './ApiHelper'
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.screen}>
-      <View >
-        <Text>Sample Api Call App</Text>
-      </View>
 
-    </SafeAreaView>
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30, textAlign: 'center' }}>
+        Example of Axios Networking in React Native Using Async Await
+      </Text>
+      {/*Running GET Request*/}
+
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={getDataUsingSimpleGetCall}>
+        <Text style={styles.textStyle}>Get Data</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={postDataUsingSimplePostCall}>
+        <Text style={styles.textStyle}>Post Data</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={multipleRequestsInSingleCall}>
+        <Text style={styles.textStyle}>Multiple Concurrent Get Requests</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
+    justifyContent: 'center',
     flex: 1,
+    padding: 16,
+  },
+  buttonStyle: {
     alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: '100%',
+    marginTop: 16,
+  },
+  textStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
 
